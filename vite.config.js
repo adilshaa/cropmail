f2b3a1e09ffd0a4a1fcace51390932a0b2c5d3e3
adilshaa <adilsha042@gmail.com>
2025-01-08 17:10:53 +0530

    Update Vite configuration; change base path to root, enhance asset file naming, and add CORS headers

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',  // This ensures assets are loaded relative to index.html
-  server: {
-    headers: {
-      'Content-Type': 'text/css',
-    },
-    middleware: ({ res }) => {
-      res.setHeader('Content-Type', 'text/css');
-    },
-  },
-  build: {
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
-            return 'assets/css/[name]-[hash][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        },
-      },
-    },
-  },
-})
-
+	plugins: [react()],
+	base: "/", // Changed to root path
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: "assets/[name].[hash][extname]",
+				chunkFileNames: "assets/[name].[hash].js",
+				entryFileNames: "assets/[name].[hash].js",
+			},
+		},
+		assetsInlineLimit: 0,
+		cssCodeSplit: false,
+		sourcemap: true,
+	},
+	server: {
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET",
+			"Access-Control-Allow-Headers": "Content-Type",
+		},
+	},
+});
