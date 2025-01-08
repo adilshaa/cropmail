dfedb4c6e33b5712b039e83f98729bbf02dacb79
adilshaa <adilsha042@gmail.com>
2025-01-08 17:03:47 +0530

    Set base path for asset loading in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',  // This ensures assets are loaded relative to index.html
 })
 
