2b320eaf3d8a4e38bf147aca6bb664392c24d974
adilshaa <adilsha042@gmail.com>
2025-01-08 17:02:14 +0530

    Refactor Vite configuration; remove structuredClone polyfill and clean up code

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// Add structuredClone polyfill
-if (typeof globalThis.structuredClone !== "function") {
-	globalThis.structuredClone = function (obj) {
-		return JSON.parse(JSON.stringify(obj));
-	};
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	assetsInclude: ['**/*.html'],
-});
+  plugins: [react()],
+})
+
