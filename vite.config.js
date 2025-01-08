4a3bb8a55a064a751f1fb2bee49c5d1834e72462
adilshaa <adilsha042@gmail.com>
2025-01-08 17:05:44 +0530

    Enhance Vite configuration; add server headers for CSS and improve asset file naming in build process

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,26 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: './',  // This ensures assets are loaded relative to index.html
+  server: {
+    headers: {
+      'Content-Type': 'text/css',
+    },
+    middleware: ({ res }) => {
+      res.setHeader('Content-Type', 'text/css');
+    },
+  },
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'assets/css/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
 })
 
